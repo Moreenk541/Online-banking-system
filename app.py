31cd71eae996244dc880f1c04cd8484fedbe42a0
moreen karimi <moreenk541@gmail.com>
2025-05-28 11:50:17 +0300

    account_typeaccount_type

---
+++ app.py
@@ -0,0 +1,52 @@
+from conn import SessionLocal
+from models import User,Bank_Account,Transcation
+from sqlalchemy.exc import IntegrityError
+
+
+session = SessionLocal()
+
+#Registering
+
+def register_user(name,email,password):
+    user = User(name=name,email=email,password=password)
+
+    try:
+        session.add(user)
+        session.commit()
+        print("user registered successfully!")
+
+    except IntegrityError:
+        session.rollback()
+        print('Email already exists.')  
+
+#logging in          
+
+def login(email, password):
+    user = session.query(User).filter_by(email=email,password=password).first()
+
+    if user:
+        print(f'Welcome, {user.name}!')
+        return user
+    else:
+        print('Invalid credentials.')
+        return None
+    
+#creating account
+
+def create_account(user, account_type):
+    account = Bank_Account(user_id=user.id, account_type=account_type)
+    session.add(account)
+    session.commit()
+    print(f'{account_type.capitalize()} account created successfully!')
+
+
+#deposit
+def deposit(account,amount):
+    account.balance  += amount 
+    transaction =Transcation(account_id =account.id, type ="deposit", amount=amount)
+    session.add(transaction)
+    session.commit()
+    print(f"Deposited ${amount} successfully.")
+
+#withdwraw
+def withdraw()    
