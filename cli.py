81c3d55f7797c600839bdf2a39a5e1734465bb79
moreen karimi <moreenk541@gmail.com>
2025-05-29 12:12:27 +0300

    enabled a user to have many accounts

--- cli.py
+++ cli.py
@@ -31,25 +31,67 @@ def main_menu():
 def user_menu(user):
     from conn import SessionLocal  
     session = SessionLocal()   
-    account = session.query(Bank_Account).filter_by(user_id=user.id).first()
-    if not account:
+    accounts = session.query(Bank_Account).filter_by(user_id=user.id).all()
+    if not accounts:
         acc_type = input("Create account - type(Savings/checking):")
-        create_account(user,acc_type)   
-        account = session.query(Bank_Account).filter_by(user_id = user.id).first()
+        create_account(user,acc_type) 
+        session.commit()  
+        accounts = session.query(Bank_Account).filter_by(user_id = user.id).all()
+    print('\n Your Accounts:')
+    for i, acc in enumerate(accounts):  
+        print(f'\n{i+1}. {acc.account_type.capitalize()} (Balance: {acc.balance})')  
+
+    choice=int (input('Select account: ')) 
+    account =accounts[choice-1]   
     while True:
-        print("\n1. View Balance\n2. Deposit\n3. Withdraw\n4. Transactions\n5. Logout")
+        print("\n1. View Balance\n2. Deposit\n3. Withdraw\n4. Transactions\n5.Switch Account \n6.Create new Account \n7Logout")
         choice = input("> ")
 
         if choice == "1":
+            
+            accounts = session.query(Bank_Account).filter_by(user_id=user.id).all()
+            print("\nYour Accounts:")
+            for i, acc in enumerate(accounts):
+                print(f"{i + 1}. {acc.account_type.capitalize()} (Balance: ${acc.balance})")
+            choice = int(input("Select account: "))
+            account = accounts[choice - 1]
+
             print(f"Balance: ${account.balance}")
+
+
         elif choice == "2":
+
+
+            accounts = session.query(Bank_Account).filter_by(user_id=user.id).all()
+            print("\nYour Accounts:")
+            for i, acc in enumerate(accounts):
+                print(f"{i + 1}. {acc.account_type.capitalize()} (Balance: ${acc.balance})")
+            choice = int(input("Select account: "))
+            account = accounts[choice - 1]
+
+
+
+
             amount = float(input("Deposit amount: "))
             deposit(session,account, amount) 
-
         elif choice == "3":
             amount = float(input("Withdraw amount: "))
             withdraw(session,account, amount)
         elif choice == "4":
             view_transactions(account)
         elif choice == "5":
-            break
+            #switch accounts
+            accounts = session.query(Bank_Account).filter_by(user_id=user.id).all()
+            print("\nYour Accounts:")
+            for i, acc in enumerate(accounts):
+                print(f"{i + 1}. {acc.account_type.capitalize()} (Balance: ${acc.balance})")
+            choice = int(input("Select account: "))
+            account = accounts[choice - 1]
+
+        elif choice =="6":
+            #new account
+            acc_type = input("Create account - type(Savings/checking):")
+            create_account(user,acc_type) 
+            session.commit() 
+
+                    
